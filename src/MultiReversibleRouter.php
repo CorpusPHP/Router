2f3f76bda2f155f481a4863a2813e153cfbf36d3
Jesse Donat <jdonat@capstonedigital.com>
2015-04-21 15:41:52 -0500

    Adds MultiRouter and MultiReversibleRouter

---
+++ src/MultiReversibleRouter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Corpus\Router;
+
+use Corpus\Router\Interfaces\ReversibleRouterInterface;
+use Corpus\Router\Interfaces\RouterInterface;
+
+class MultiReversibleRouter extends MultiRouter {
+
+	/**
+	 * Add a router to the queue
+	 *
+	 * @param \Corpus\Router\Interfaces\ReversibleRouterInterface $router
+	 */
+	public function addRouter( RouterInterface $router ) {
+		if( $router instanceof ReversibleRouterInterface ) {
+			return parent::addRouter($router);
+		}
+
+		throw new \InvalidArgumentException('Expected ReversibleRouterInterface');
+	}
+
+	/**
+	 * Loops over routers in the order they were added until a generated URL is found.
+	 *
+	 * @param string|object $controller Instance or Relative 'admin\index' or absolute '\Controllers\www\admin\index'
+	 * @param string|null   $action
+	 * @param array         $options
+	 * @return string|false
+	 */
+	public function generate( $controller, $action = null, array $options = [ ] ) {
+		/**
+		 * @var $router ReversibleRouterInterface
+		 */
+		foreach( $this->routers as $router ) {
+			$link = $router->generate($controller, $action, $options);
+
+			if( $link !== false ) {
+				return $link;
+			}
+		}
+
+		return false;
+	}
+}
---
+++ src/MultiRouter.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Corpus\Router;
+
+use Corpus\Router\Interfaces\RouterInterface;
+
+/**
+ * MultiRouter
+ *
+ * @package Corpus\Router
+ */
+class MultiRouter implements RouterInterface {
+
+	/**
+	 * @var RouterInterface[]
+	 */
+	protected $routers = [ ];
+
+	/**
+	 * @param ... RouterInterface
+	 */
+	public function __construct() {
+		foreach( func_get_args() as $arg ) {
+			$this->addRouter($arg);
+		}
+	}
+
+	/**
+	 * Add a router to the queue
+	 *
+	 * @param \Corpus\Router\Interfaces\RouterInterface $router
+	 */
+	public function addRouter( RouterInterface $router ) {
+		$this->routers[] = $router;
+	}
+
+	/**
+	 * @return Interfaces\RouterInterface[]
+	 */
+	public function getRouters() {
+		return $this->routers;
+	}
+
+	/**
+	 * Loops over routers in the order they were added until a match is found.
+	 *
+	 * @param string $path
+	 * @return array|false
+	 */
+	public function match( $path ) {
+		foreach( $this->routers as $router ) {
+			$match = $router->match($path);
+			
+			if( $match !== false ) {
+				return $match;
+			}
+		}
+
+		return false;
+	}
+}
